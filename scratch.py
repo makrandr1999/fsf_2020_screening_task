86b3d708e20377e70562477cd186dcf5f386ee10
makrandr1999 <makrand.r@somaiya.edu>
2020-01-23 22:48:47 +0530

    Modify load input function to populate all 4 tabs with data from the input file

--- scratch.py
+++ scratch.py
@@ -11,46 +11,41 @@ class MyForm(QMainWindow,Ui_MainWindow):
         self.ui.btn_load_input.clicked.connect(self.loadInputs)
         self.show()
 
-    def addDummyData(self,path):
-        # for i in range(10):
-        #     for j in range(10):
-        #         self.ui.tableWidget.setItem(i,j,QTableWidgetItem(str(i))) 
-        wb = xlrd.open_workbook(path) 
-        sheet = wb.sheet_by_index(0) 
-        sheet_names = wb.sheet_names()
-        self.ui.tableWidget.setRowCount(sheet.nrows)
+    def loadData(self,path):
+        self.wb = xlrd.open_workbook(path) # 
+        sheet_names = self.wb.sheet_names()
+        no_of_sheets=len(sheet_names)
+        for i in range(no_of_sheets):
+            self.loadIntoTable(self.wb.sheet_by_index(i),sheet_names[i]);
 
-        # print(sheet_names)  
-        # For row 0 and column 0 
-        # sheet.cell_value(0, 0) 
-        # self.ui.tableWidget.setItem(0,0,QTableWidgetItem(str(0)))  
-        for i in range(1,sheet.nrows): 
-            for j in range(7): 
-                # print(sheet.cell_value(i,j),i,j)
-                self.ui.tableWidget.setItem(i-1,j,QTableWidgetItem(str(sheet.cell_value(i,j))))  
-                # self.ui.tableWidget.setItem(i-1,j,QTableWidgetItem(str(0)))
-
-
-
-
-
+    def loadIntoTable(self,curr_sheet,sheet_name):       
+        sheets_dict={ 'FinPlate': {'table_name': 'table_fin', 'cols': '7'}, 
+         'TensionMember': {'table_name': 'table_ten', 'cols': '5'},'BCEndPlate': {'table_name': 'table_bce', 'cols': '8'},
+         'CleatAngle': {'table_name': 'table_cleat', 'cols': '7'}}
+        tablename=sheets_dict[sheet_name]['table_name']
+        tablecols=int(sheets_dict[sheet_name]['cols'])
+        table = (self.findChild(QTableWidget,tablename))          
+        table.setRowCount(curr_sheet.nrows-1)
+        for i in range(1,curr_sheet.nrows): 
+            for j in range(int(tablecols)): 
+                table.setItem(i-1,j,QTableWidgetItem(str(curr_sheet.cell_value(i,j))))  
 
     def loadInputs(self):
-        self.addDummyData('/home/makishere/fossee2020/Input_values.xlsx')
-        # fname = QFileDialog.getOpenFileName(self, 'Open file', '/home/makishere/fossee2020')
-        # extension = os.path.splitext(fname[0])[1]
-        # # print(fname[0])
-        # supportedFileTypes=['.csv','.xlsx']
-        # if (extension in supportedFileTypes):
-        #     self.addDummyData(fname[0])
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home/makishere/fossee2020')
+        extension = os.path.splitext(fname[0])[1]
+        # print(fname[0])
+        supportedFileTypes=['.csv','.xlsx']
+        if (extension in supportedFileTypes):
+            self.loadData(fname[0])
+            # self.addDummyData(fname[0])
 
-        # else:
-        #     msg = QMessageBox()
-        #     msg.setIcon(QMessageBox.Critical)
-        #     msg.setText("File Type Not Supported")
-        #     msg.setInformativeText('Please load a CSV or an xlsx file')
-        #     msg.setWindowTitle("Error")
-        #     msg.exec_()
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("File Type Not Supported")
+            msg.setInformativeText('Please load a CSV or an xlsx file')
+            msg.setWindowTitle("Error")
+            msg.exec_()
 
             # app.exec_()
       
