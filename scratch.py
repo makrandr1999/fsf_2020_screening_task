95d9c1fd6f86da469919357580cc2fabf02cb13c
makrandr1999 <makrand.r@somaiya.edu>
2020-01-26 14:37:22 +0530

    Add validate function to check if all the IDs are unique and if all cell values are numeric

--- scratch.py
+++ scratch.py
@@ -9,26 +9,74 @@ class MyForm(QMainWindow,Ui_MainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.btn_load_input.clicked.connect(self.loadInputs)
+        self.ui.btn_validate.clicked.connect(self.validate)
         self.show()
+    def dispMessage(self,text,informativeText,WindowTitle):
+            msg = QMessageBox()
+            # msg.setIcon(QMessageBox.Critical)
+            msg.setText(text)
+            msg.setInformativeText(informativeText)
+            msg.setWindowTitle(WindowTitle)
+            msg.exec_()   
+    def validate(self):
+        tablenames=['table_fin','table_ten','table_bce','table_cleat']
+        for sheet_name in self.sheet_names:
+            tablename=self.sheets_dict[sheet_name]['table_name']
+            print(self.validateNumeric(tablename))
+            # self.validateNumeric(tablename)
+        # self.dispMessage('lol','lol','lol')    
+    def validateID(self,tablename):
+        table = (self.findChild(QTableWidget,tablename)) 
+        allRows = table.rowCount()
+        ids=[]
+        for row in range(0,allRows):
+            y=table.item(row,0).text()
+            # print(y)
+            ids.append(y)
+        s =set()
+        for x in ids:
+            if x in s: return False
+            s.add(x)
+        return True
+
+    def validateNumeric(self,tablename):
+        table = (self.findChild(QTableWidget,tablename))
+        allRows = table.rowCount()
+        # int flag=0;
+        tablecols=table.columnCount()
+        for i in range(0,allRows): 
+            for j in range(tablecols): 
+                try:
+                    float(table.item(i,j).text())
+                    # return True
+                except ValueError:
+                    return False        
+            
+        return True            
+
+
+
+
+            
 
     def loadData(self,path):
         self.wb = xlrd.open_workbook(path) # 
-        sheet_names = self.wb.sheet_names()
-        no_of_sheets=len(sheet_names)
+        self.sheet_names = self.wb.sheet_names()
+        no_of_sheets=len(self.sheet_names)
         for i in range(no_of_sheets):
-            self.loadIntoTable(self.wb.sheet_by_index(i),sheet_names[i]);
+            self.loadIntoTable(self.wb.sheet_by_index(i),self.sheet_names[i]);
 
     def loadIntoTable(self,curr_sheet,sheet_name):       
-        sheets_dict={ 'FinPlate': {'table_name': 'table_fin', 'cols': '7'}, 
+        self.sheets_dict={ 'FinPlate': {'table_name': 'table_fin', 'cols': '7'}, 
          'TensionMember': {'table_name': 'table_ten', 'cols': '5'},'BCEndPlate': {'table_name': 'table_bce', 'cols': '8'},
          'CleatAngle': {'table_name': 'table_cleat', 'cols': '7'}}
-        tablename=sheets_dict[sheet_name]['table_name']
-        tablecols=int(sheets_dict[sheet_name]['cols'])
+        tablename=self.sheets_dict[sheet_name]['table_name']
+        tablecols=int(self.sheets_dict[sheet_name]['cols'])
         table = (self.findChild(QTableWidget,tablename))          
         table.setRowCount(curr_sheet.nrows-1)
         for i in range(1,curr_sheet.nrows): 
             for j in range(int(tablecols)): 
-                table.setItem(i-1,j,QTableWidgetItem(str(curr_sheet.cell_value(i,j))))  
+                table.setItem(i-1,j,QTableWidgetItem(str(curr_sheet.cell_value(i,j))))
 
     def loadInputs(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file', '/home/makishere/fossee2020')
